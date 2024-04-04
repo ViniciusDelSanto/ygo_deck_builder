2e5738bb76e8523e7166cbe2c38521badd5dd234
Vinicius <vinimail7@gmail.com>
2024-04-04 01:09:15 -0300

    Tentando criar models, templates e views novas para as telas necessarias

--- ygo_deck_builder/views.py
+++ ygo_deck_builder/views.py
@@ -1,5 +1,6 @@
 import requests
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Deck
 
 def card_info(request):
     card_name = request.GET.get('card_name')
@@ -24,3 +25,37 @@ def all_cards(request):
     data = response.json()
     card_names = [card['name'] for card in data['data']]
     return render(request, 'all_cards.html', {'card_names': card_names})
+
+def list_decks(request):
+    decks = Deck.objects.all() 
+    return render(request, 'list_decks.html', {'decks': decks})
+
+def create_or_edit_deck(request, deck_id=None):
+    deck = None
+    if deck_id:
+        deck = get_object_or_404(Deck, id=deck_id)
+    
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        if deck:
+            deck.name = name
+            deck.save()
+        else:
+            Deck.objects.create(name=name)
+        return redirect('list_decks')
+    
+    # Obter todos os cards da API para exibir na div da direita
+    url = "https://db.ygoprodeck.com/api/v7/cardinfo.php"
+    response = requests.get(url)
+    cards_data = response.json()['data']
+    card_names = [card['name'] for card in cards_data]
+
+    return render(request, 'create_edit_deck.html', {'deck': deck, 'card_names': card_names})
+
+
+def delete_deck(request, deck_id):
+    deck = get_object_or_404(Deck, id=deck_id)
+    if request.method == 'POST':
+        deck.delete()
+        return redirect('list_decks')
+    return render(request, 'delete_deck.html', {'deck': deck})
